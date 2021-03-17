e48a4cd8d03afd399bcc986bd5ff6f15b2129348
Jens Bouma <jensmbouma@kpnmail.nl>
2021-03-17 14:01:57 +0100

    Kleine aanpassingen aan de button

--- components/Button/index.js
+++ components/Button/index.js
@@ -4,8 +4,8 @@ import {TouchableOpacity} from 'react-native-gesture-handler';
 
 export default function Button({style, onPress, title}) {
     return (
-        <TouchableOpacity style={style} onPress={onPress}>
-            <Text style={{color: style?.color, fontSize: style?.fontSize, fontWeight: style?.fontWeight, textTransform: style?.textTransform}}>{title}</Text>
+        <TouchableOpacity style={style ? style : null} onPress={onPress ? onPress : null}>
+            <Text style={{color: style?.color, fontSize: style?.fontSize, fontWeight: style?.fontWeight, textTransform: style?.textTransform}}>{title ? title : 'Geen titel gezet'}</Text>
         </TouchableOpacity>
     )
 }
