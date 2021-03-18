cdc21fdb629f27641f7a44c81186f214e5b06a94
Jens Bouma <jensmbouma@kpnmail.nl>
2021-03-18 11:26:04 +0100

    Button props updated.

--- components/Button/index.js
+++ components/Button/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import {Text} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 
-export default function Button({style, onPress, title}) {
+export default function Button({style, onPress, title, children}) {
     return (
         <TouchableOpacity style={style ? style : null} onPress={onPress ? onPress : null}>
-            <Text style={{color: style?.color, fontSize: style?.fontSize, fontWeight: style?.fontWeight, textTransform: style?.textTransform}}>{title ? title : 'Geen titel gezet'}</Text>
+            {title ?
+                <Text style={{
+                    color: style?.color,
+                    fontSize: style?.fontSize,
+                    fontWeight: style?.fontWeight,
+                    textTransform: style?.textTransform
+                }}>{title ? title : null}</Text>
+            : null}
+                {children}
         </TouchableOpacity>
     )
 }
