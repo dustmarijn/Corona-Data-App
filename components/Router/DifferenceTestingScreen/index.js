728279d1297c6b71c3fbb541148588a3e97c6d26
Janick Dragt <jdragt7697@student.landstede.nl>
2021-04-09 15:32:53 +0200

    merge

--- components/Router/DifferenceTestingScreen/index.js
+++ components/Router/DifferenceTestingScreen/index.js
@@ -10,7 +10,7 @@ export default function DifferenceTestingScreen({navigation}) {
     const [filteredData, setFilteredData] = useState('Hallo');
     const [usePositiveData, setUsePositiveData] = useState(0);
     const [useTestedResult, setUseTestedResult] = useState(0);
-    const [useZiekenData, setUseZiekenData] = useState(0);
+    const [useSickData, setUseSickData] = useState(0);
     const [useDeathData, setUseDeathData] = useState(0);
 
     const {data} = ApiData();
@@ -18,7 +18,7 @@ export default function DifferenceTestingScreen({navigation}) {
     const {hospitalData} = ApiHospital();
 
     function GetDataApi() {
-        if (data !== null && hospitalData !== null) {
+        if (data !== null) {
 
             let startDate = '2020-12-16'; // Start particuliere testen
             let endDate = '2021-01-06'; // Einde particuliere testen
@@ -27,12 +27,12 @@ export default function DifferenceTestingScreen({navigation}) {
                 return obj.Date_of_statistics >= startDate && obj.Date_of_statistics <= endDate;
             });
 
-            let hospitalFilteredData = hospitalData.filter((obj) => {
-                return obj.Date_of_publication >= startDate && obj.Date_of_publication <= endDate;
-            });
+            // let hospitalFilteredData = hospitalData.filter((obj) => {
+            //     return obj.Date_of_publication >= startDate && obj.Date_of_publication <= endDate;
+            // });
 
-            let ziekenData = useZiekenData;
-            let deathData = useDeathData;
+            // let SickData = useSickData;
+            // let deathData = useDeathData;
 
             // filter 1 dag op deze manier: const filteredData = data.filter((item => item.Date_of_statistics === '2020-12-17'));
             let testedResult = useTestedResult;
@@ -43,12 +43,12 @@ export default function DifferenceTestingScreen({navigation}) {
                 positiveData += Tested_positive;
             }
 
-            for (const {Hospital_admission, Deceased} of hospitalFilteredData) {
-                ziekenData += Hospital_admission;
-                deathData += Deceased;
-            }
-            setUseZiekenData(ziekenData);
-            setUseDeathData(deathData);
+            // for (const {Hospital_admission, Deceased} of hospitalFilteredData) {
+            //     SickData += Hospital_admission;
+            //     deathData += Deceased;
+            // }
+            // setUseSickData(SickData);
+            // setUseDeathData(deathData);
             setUseTestedResult(testedResult);
             setUsePositiveData(positiveData);
         }
@@ -110,7 +110,7 @@ export default function DifferenceTestingScreen({navigation}) {
             <Text  style={{fontSize: 15}}> Datum van 16 December tot 06 Januari. </Text>
             <Text> Aantal testen: {useTestedResult ? useTestedResult : '0'} </Text>
             <Text> Postive testen: {usePositiveData ? usePositiveData : '0'} </Text>
-            <Text> Ziekenhuis opnames: {useZiekenData ? useZiekenData : '0'} </Text>
+            <Text> Ziekenhuis opnames: {useSickData ? useSickData : '0'} </Text>
             <Text> Overlden: {useDeathData ? useDeathData : '0'} </Text>
 
             <Text>  </Text>
